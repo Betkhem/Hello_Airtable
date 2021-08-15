cbf3fcab3b43f62d3fbf49e965a2860fd135fb01
Betkhem <54867749+Betkhem@users.noreply.github.com>
2021-08-15 20:14:55 +0300

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,9 +1,9 @@
 import requests
 
-def lambda_handler(event, context):
-    AIRTABLE_BASE_ID = "appfYvHhxoN5sQjm8"
-    AIRTABLE_TABLE_NAME = "MainTable"
-    endpoint = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
+def lambda_handler():
+    AIRTABLE_BASE_ID = "shrp5Xhe3bkzWkkhz"
+    AIRTABLE_TABLE_NAME = "tblX1jGmYZoqyHrlE"
+    endpoint = "https://api.airtable.com/v0/appS2PargbyUuC55c/MainTable"
     
     headers = {
         "Authorization":"Bearer keyGBZTg6leNOpAxw",
@@ -25,5 +25,8 @@ def lambda_handler(event, context):
         }
       ]
     }
-    r = requests.post(endpoint, json=data, headers=headers)
-    return r.json()
+    r = requests.get(endpoint, json=data, headers=headers)
+    a = []
+    res = r.json()
+    b = [res['records'][i]['fields']['title'] for i in range(len(res['records']))]
+    return b

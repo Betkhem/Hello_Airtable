361517b1b114e572e75613327d54c681c189b857
Betkhem <54867749+Betkhem@users.noreply.github.com>
2021-08-16 01:42:05 +0300

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,6 +1,6 @@
 import requests
 
-def lambda_handler():
+def lambda_handler(event, context):
     AIRTABLE_BASE_ID = "shrp5Xhe3bkzWkkhz"
     AIRTABLE_TABLE_NAME = "tblX1jGmYZoqyHrlE"
     endpoint = "https://api.airtable.com/v0/appS2PargbyUuC55c/MainTable"
@@ -10,23 +10,40 @@ def lambda_handler():
         "Content-Type": "application/json",
     }
     data = {
-      "records": [
-        {
-          "fields": {
-            "ID": 1,
-            "title": "Проверка 1"
-          }
-        },
-        {
-          "fields": {
-            "ID": 2,
-            "title": "Проверка 2"
-          }
-        }
-      ]
+        "records": [
+            {
+                "id": "rec7yqd3qgWKb3C3L",
+                "fields": {
+                    "ID": 1,
+                    "title": "Проверка 1"
+                },
+                "createdTime": "2021-08-15T16:58:24.000Z"
+            },
+            {
+                "id": "recqfBklgbBUZxegE",
+                "fields": {
+                    "ID": 2,
+                    "title": "Проверка 2"
+                },
+                "createdTime": "2021-08-15T16:58:24.000Z"
+            },
+            {
+                "id": "recMSRE5shQZgx9TG",
+                "fields": {
+                    "ID": 3,
+                    "title": "Проверка 3"
+                },
+                "createdTime": "2021-08-15T16:58:24.000Z"
+            }
+        ],
+        "offset": "recMSRE5shQZgx9TG"
     }
     r = requests.get(endpoint, json=data, headers=headers)
-    a = []
     res = r.json()
-    b = [res['records'][i]['fields']['title'] for i in range(len(res['records']))]
-    return b
+    c = [(res['records'][i]['fields']['ID'], res['records'][i]['fields']['title']) for i in range(len(res['records']))]
+    d = [c[i][1] for i in range(len(c))]
+    for i in range(len(c)):
+        if c[i][0] == 1:
+            d[0] = d[0].replace(d[0], c[i][1], 1)
+        d[c[i][0] - 1] = d[c[i][0] - 1].replace(d[c[i][0] -1 ], c[i][1])
+    return d
